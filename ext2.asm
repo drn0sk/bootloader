bf2a44f4116bcbc8b8024d0666dd6ca6892599f5
drn0sk <DarshanS2912@gmail.com>
2025-03-01 14:52:34 -0500

    edited some comments in ext2.asm

--- ext2.asm
+++ ext2.asm
@@ -8,14 +8,12 @@
 ;carry flag set on error
 
 ; ext2_load_file
-;drive number in bl
 ;ds:si -> path
 ;cx is the length of the path
 ;es:di -> buffer big enough to hold file
 ;Carry Flag (CF) set on error
 
 ; ext2_load_file_relative
-;drive number in bl
 ;ds:si -> path
 ;cx is the length of the path
 ;es:di -> any necessary info about parent directory
@@ -23,7 +21,6 @@
 ;Carry Flag (CF) set on error
 
 ; ext2_get_file_size
-;drive number in bl
 ;ds:si -> path
 ;cx is the length of the path
 ;returns file size in eax
